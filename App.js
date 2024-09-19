f0bfa9271bc53d67187a5b4dd7255134611fa9c2
Sunny Kumar Singh <SRajukumarsingh@ivycomptech.com>
2024-09-19 22:14:53 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,23 @@
+/*
+<div id="parent">
+    <div id="child1">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+    </div>
+    <div id="child2">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+    </div>
+</div>
+*/
+
+const parent = React.createElement("div", {id:"parent"},[
+     React.createElement("div",{id:"child1"}, [
+     React.createElement("h1",{},"Heading 1"), React.createElement("h2",{},"Heading 2")
+    ]),
+     React.createElement("div",{id:"child2"}, [
+     React.createElement("h1",{},"Heading 1"), React.createElement("h2",{},"Heading 2")])
+    ])
+
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+    root.render(parent)
